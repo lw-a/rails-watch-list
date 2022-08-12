e654faa804a7893f85a7006058a9faba8532d8ab
Adam Hemming <hemming.dev@gmail.com>
2022-08-12 16:52:39 +0900

    validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   # has_many :lists, through: :bookmarks
 
-  validates :title, uniqueness: true, presence: true
+  # validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
 end
